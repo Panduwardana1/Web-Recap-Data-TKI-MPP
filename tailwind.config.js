327932728accdbf005d1a1e5f57224acaa98bbc7
Panduwardana <ochaxtt@gmail.com>
2025-08-14 23:56:00 +0800

    tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,36 @@
-/** @type {import('tailwindcss').Config} */
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     "./resources/**/*.blade.php",
+//     "./resources/**/*.js",
+//     "./resources/**/*.vue",
+//   ],
+//   theme: {
+//     extend: {
+//         fontFamily: {
+//             // manrope
+//             manrope : ['Manrope', 'sans-serif'],
+//             // urbanist
+//             urbanist : ['Usrbanist', 'sans-serif'],
+//         },
+//     },
+//   },
+//   plugins: [],
+// }
+
 export default {
   content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
+    './resources/**/*.blade.php',
+    './resources/**/*.js',
+    './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+    './storage/framework/views/*.php',
   ],
   theme: {
     extend: {
-        fontFamily: {
-            // manrope
-            manrope : ['Manrope', 'sans-serif'],
-            // urbanist
-            urbanist : ['Usrbanist', 'sans-serif'],
-        },
+      fontFamily: {
+        manrope: ['Manrope', 'sans-serif'],
+        urbanist: ['Urbanist', 'sans-serif'],
+      },
     },
   },
   plugins: [],
