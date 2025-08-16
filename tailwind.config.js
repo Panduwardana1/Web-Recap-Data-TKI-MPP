a4a8553036109af75ff614891c6122805d1734cb
Panduwardana <ochaxtt@gmail.com>
2025-08-16 10:30:53 +0800

    Tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,15 @@
-// /** @type {import('tailwindcss').Config} */
-// export default {
-//   content: [
-//     "./resources/**/*.blade.php",
-//     "./resources/**/*.js",
-//     "./resources/**/*.vue",
-//   ],
-//   theme: {
-//     extend: {
-//         fontFamily: {
-//             // manrope
-//             manrope : ['Manrope', 'sans-serif'],
-//             // urbanist
-//             urbanist : ['Usrbanist', 'sans-serif'],
-//         },
-//     },
-//   },
-//   plugins: [],
-// }
-
+/** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './resources/**/*.blade.php',
-    './resources/**/*.js',
-    './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-    './storage/framework/views/*.php',
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.php",
   ],
   theme: {
     extend: {
-      fontFamily: {
-        manrope: ['Manrope', 'sans-serif'],
-        urbanist: ['Urbanist', 'sans-serif'],
-      },
+        fontFamily : {
+            manrope : ['Manrope', 'sans-serif'],
+        }
     },
   },
   plugins: [],
