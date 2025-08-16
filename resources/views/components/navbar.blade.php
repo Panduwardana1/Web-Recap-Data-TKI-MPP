77ae8497d882c82f1d80fe96a9283d4bd4cb6657
Panduwardana <ochaxtt@gmail.com>
2025-08-16 20:41:05 +0800

    navbar

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,7 @@
+<div class="flex justify-between px-6 py-4 items-center fixed top-0 left-0 z-50 w-full font-manrope bg-white">
+        <h1 class="text-xl font-semibold">LOGO MPP</h1>
+        <ul class="font-semibold">
+            {{ $slot }}
+        </ul>
+        <button class="font-semibold">Login</button>
+    </div>
