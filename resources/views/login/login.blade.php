c7495f24487ef4d925af20f6beaa99ce34c2873e
Panduwardana <ochaxtt@gmail.com>
2025-08-17 16:54:10 +0800

    login

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -1,25 +1,29 @@
-@extends('landingpage')
+{{-- @extends('landingpage') --}}
 
-@section('login')
-    <div class="items-center font-manrope flex-wrap p-6 rounded-md border shadow-sm">
-            <h1 class="font-bold text-center text-3xl">Login to your account</h1>
-        <div class="py-4">
-            <form action="" class="space-y-4 flex flex-col items-center justify-center">
-                <div class="min-w-full">
-                    <p class="font-semibold pb-2">Username</p>
-                    <input type="text" name="username" class="pl-2 w-full p-1 rounded-md ring-1 ring-slate-200" placeholder="Username">
+{{-- @section('login') --}}
+<div class="items-center font-manrope flex-wrap p-12 bg-white rounded-md border shadow-sm" id="login">
+    <h1 class="font-bold text-center text-3xl">Login to your account</h1>
+    <div class="py-4">
+        <form action="" class="space-y-4 flex flex-col items-center justify-center select-none">
+            <div class="min-w-full">
+                <p class="font-semibold pb-2">Username</p>
+                <input type="text" name="username" class="pl-2 w-full p-1 rounded-md ring-1 ring-slate-200"
+                    placeholder="Username">
+            </div>
+
+            <div class="min-w-full">
+                <div class="flex items-center justify-between pb-2">
+                    <p class="font-semibold">Password</p>
+                    <a href="#" class="font-semibold hover:text-blue-600 underline">Forgot Password</a>
                 </div>
+                <input type="password" name="password" class="pl-2 w-full p-1 rounded-md ring-1 ring-slate-200"
+                    placeholder="Password">
+            </div>
 
-                <div class="min-w-full">
-                    <div class="flex items-center justify-between pb-2">
-                        <p class="font-semibold">Password</p>
-                        <a href="#" class="font-semibold">Forgot Password</a>
-                    </div>
-                    <input type="password" name="password" class="pl-2 w-full p-1 rounded-md ring-1 ring-slate-200" placeholder="Password">
-                </div>
-
-                <button class="py-2 min-w-full rounded-md bg-teal-600 hover:bg-teal-700 text-white font-semibold">Login</button>
-            </form>
-        </div>
+            <button
+                class="py-2 min-w-full rounded-md bg-blue-600 hover:bg-blue-700 text-white font-semibold">Login</button>
+        </form>
     </div>
-@endsection
+</div>
+<script src="{{ asset('js/app.js') }}"></script>
+{{-- @endsection --}}
