03111a5829fe5a30653f16e74742cbbf9d8669d0
Panduwardana <ochaxtt@gmail.com>
2025-08-17 16:54:47 +0800

    navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,7 +1,24 @@
-<div class="flex justify-between px-6 py-4 items-center fixed top-0 left-0 z-50 w-full font-manrope bg-white">
-        <h1 class="text-xl font-semibold">LOGO MPP</h1>
-        <ul class="font-semibold">
-            {{ $slot }}
-        </ul>
-        <button class="font-semibold">Login</button>
+<div class="flex justify-between px-6 py-4 items-center fixed top-0 left-0 z-50 w-full font-manrope bg-white p-2">
+    <h1 class="text-xl font-semibold">SIREDA</h1>
+    <div class="flex gap-4 font-semibold items-center">
+        <div><a href="/home"
+                class="px-3 py-1 max-w-full hover:bg-slate-800 hover:text-white rounded-md ease-in duration-75">Home</a>
+        </div>
+        <div><a href="/about"
+                class="flex px-3 py-1 max-w-full hover:bg-slate-800 hover:text-white rounded-md ease-in duration-75 gap-1">About <i data-feather="chevron-down" class="text-black"></i> </a>
+        </div>
+        <div><a href="/docs"
+                class="px-3 py-1 max-w-full hover:bg-slate-800 hover:text-white rounded-md ease-in duration-75">Docs</a>
+        </div>
+        <div><a href="/tutorials"
+                class="px-3 py-1 max-w-full hover:bg-slate-800 hover:text-white rounded-md ease-in duration-75">Tutorials</a>
+        </div>
     </div>
+    <div class="flex items-center gap-2 font-semibold">
+        <p>Profile Name</p>
+        <div class="p-4 rounded-full bg-black"></div>
+    </div>
+    {{-- <button
+        class="font-semibold px-3 py-1 max-w-full bg-blue-600 hover:bg-blue-800 text-white rounded-md ease-in duration-75 active:bg-black"
+        id="btn-login">Login</button> --}}
+</div>
