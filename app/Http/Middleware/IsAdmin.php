4d8fe97f3fef8427ec1ae257932948610a944b29
Panduwardana <ochaxtt@gmail.com>
2025-08-18 21:07:53 +0800

    admin-middleware

---
+++ app/Http/Middleware/IsAdmin.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use view;
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
+
+class IsAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+
+        if (Auth::check() && Auth::User()->role === 'admin') {
+            return $next($request);
+        } else {
+            abort(403, 'Cannot Access');
+        }
+    }
+}
