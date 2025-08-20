e4c24c615501b896adfe9a3e674b5329661bc895
Panduwardana <ochaxtt@gmail.com>
2025-08-20 13:46:35 +0800

    seeder

---
+++ database/seeders/TkiSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Tki;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TkiSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Tki::factory()->count(100)->create();
+    }
+}
